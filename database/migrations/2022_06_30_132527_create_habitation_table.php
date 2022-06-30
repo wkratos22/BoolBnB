bc3e3d15e41d5c4ac88a00c15fc2d2dac2d7c38f
simoneluca00 <si.zaino@libero.it>
2022-06-30 16:01:12 +0200

    Migrations added

---
+++ database/migrations/2022_06_30_132527_create_habitation_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHabitationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('habitation', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->mediumText('description');
+            $table->decimal('price', 7,2);
+            $table->string('address');
+            $table->decimal('latitude', 7,5);
+            $table->decimal('longitude', 8,5);
+            $table->integer('guests_number');
+            $table->integer('rooms_number');
+            $table->integer('beds_number');
+            $table->integer('bathrooms_number');
+            $table->integer('square_meters')->nullable();
+            $table->string('image');
+            $table->boolean('visible');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('habitation');
+    }
+}
