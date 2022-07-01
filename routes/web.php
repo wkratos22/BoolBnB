21c25b1b54dd65c7333848e6d46c32efba542428
ClaraNocilla <claranocilla12@gmail.com>
2022-07-01 13:22:09 +0200

    vue main components, guest route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
