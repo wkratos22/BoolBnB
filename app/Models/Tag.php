a653be2b364bb1d1bf5448f62f1e25584e022c0e
Samuele <spadaro.samuele@gmailcom>
2022-07-01 12:35:12 +0200

    Tag seeder

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class HabitationTag extends Model
+class Tag extends Model
 {
     protected $fillable = [
         'label', 'icon'
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call(
             [
                 HabitationTypeSeeder::class,
-                HabitationSeeder::class
+                HabitationSeeder::class,
+                TagSeeder::class
             ]
     
     );
