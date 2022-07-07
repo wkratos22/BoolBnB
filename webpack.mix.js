4f60592d2ca91fab07c0f93e62db14eccde27a23
wkratos22 <michaelwachnicki2001@gmail.com>
2022-07-07 16:35:55 +0200

    footer vue finito

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,10 @@ mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/front.js', 'public/js')
     .js('resources/js/routes.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .copy(
+        'node_modules/@fortawesome/fontawesome-free/webfonts',
+        'public/webfonts'
+    )
     .options({
         processCssUrls: false
     });
