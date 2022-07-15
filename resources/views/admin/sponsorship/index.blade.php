aeb1f73f0d3a87959ef949b3f56720921aa9fae0
Samuele <spadaro.samuele@gmailcom>
2022-07-15 18:35:40 +0200

    stile sponsorship fatto

--- resources/views/admin/sponsorship/index.blade.php
+++ resources/views/admin/sponsorship/index.blade.php
@@ -2,23 +2,26 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+<div class="container ">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 align-items-center h-100">
         @foreach ($sponsorships as $sponsorship)
-            <div class="col">
-                <div class="card my-2" style="width: 18rem;">
-                    <div class="card-body">
-                    <h5 class="card-title">{{$sponsorship->name}}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Durata: {{$sponsorship->duration}} ore</h6>
-                    <h6 class="card-text">Prezzo: {{$sponsorship->price}}€</h6>
-                    <div class="text-right">
-                        <a href="{{ route('admin.pay', [$habitation, $sponsorship])}}" class="btn btn-primary">Sponsorizza</a>
-                    </div>
-                    
-                    </div>
+        <div class="col">
+            <div class="card text-center c_border rounded_lg">
+                <div class="card-header bg-white c_border rounded_top">
+                    <h4 class="card-title">{{$sponsorship->name}}</h4>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Durata: {{$sponsorship->duration}} ore</h5>
+                  <h6 class="card-text">Prezzo: {{$sponsorship->price}}€</h6>
+                  <div class="pt-4">
+                    <a href="{{ route('admin.pay', [$habitation, $sponsorship])}}" class="btn color_button">Sponsorizza</a> 
+                  </div>
+                  
                 </div>
             </div>
-        @endforeach 
+        </div>
+        @endforeach
     </div>
 </div>
 @endsection
+
