ee111516cd63cca9aeb8ea1d94bff5651a31a9c0
Simone Saputo <simonesaputo0@gmail.com>
2022-07-17 21:29:16 +0200

    adjustment filter show front

--- resources/views/admin/habitations/show.blade.php
+++ resources/views/admin/habitations/show.blade.php
@@ -57,9 +57,9 @@
                 <td>
 
                     <!-- Modal -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
+                    <div class="modal-custom fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog-custom" role="document">
+                            <div class="modal-content-custom">
                                 <div class="modal-header text-white bg-green">
                                     {{-- <h5 class="modal-title" id="exampleModalLabel">Impostazioni</h5> --}}
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
